230d2ba33c89f13ff605881ff30188ea84ab3448
farrasnazhif <farrasnazhif@gmail.com>
2026-03-23 14:28:21 +0700

    feat: server-sync & client

---
+++ client.py
@@ -0,0 +1,96 @@
+import socket
+import os
+
+HOST = "127.0.0.1"
+PORT = 8080
+BUFFER_SIZE = 1024
+
+def receive_full(sock, size):
+    data = b""
+    while len(data) < size:
+        chunk = sock.recv(BUFFER_SIZE)
+        if not chunk:
+            break
+        data += chunk
+    return data
+
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
+
+    try:
+        while True:
+            command = input("Command: ").strip()
+
+            if not command:
+                continue
+            
+            if command.startswith("/list"):
+              client.sendall(command.encode())
+              data = client.recv(BUFFER_SIZE)
+              print("Files on server: ")
+              print(data.decode())
+
+            elif command.startswith("/upload"):
+              parts = command.split()
+              if len(parts) < 2:
+                print("Format usage: /upload <filename>")
+                continue
+              
+              filename = parts[1]
+
+              # send command and wait till got ack from server
+              client.sendall(command.encode())
+
+              filesize = os.path.getsize(filename)
+              client.sendall(str(filesize).encode())
+
+              client.recv(BUFFER_SIZE)
+
+              with open(filename, "rb") as f:
+                while True:
+                    chunk = f.read(BUFFER_SIZE)
+                    if not chunk:
+                      break
+                    client.sendall(chunk)
+
+              result = client.recv(BUFFER_SIZE)
+              print(result.decode())
+
+            elif command.startswith("/download"):
+              parts = command.split()
+              if len(parts) < 2:
+                  print("Format usage: /download <filename>")
+                  continue
+              
+              filename = parts[1]
+
+              client.sendall(command.encode())
+
+              size_data = client.recv(BUFFER_SIZE)
+              filesize = int(size_data.decode())
+              
+              # send ack
+              client.sendall(b"ok\n")
+
+              data = receive_full(client, filesize)
+
+              with open(filename, "wb") as f:
+                f.write(data)
+
+              result = client.recv(BUFFER_SIZE)
+              print(result.decode())
+
+            else:
+              client.sendall(command.encode())
+              data = client.recv(BUFFER_SIZE)
+              print(data.decode())
+
+    except KeyboardInterrupt:
+      print("\nConnection closed")
+
+    finally:
+       client.close()
+
+if __name__ == "__main__":
+    main()
---
+++ server-sync.py
@@ -0,0 +1,111 @@
+import socket
+import os
+
+HOST = "0.0.0.0"
+PORT = 8080
+BUFFER_SIZE = 1024
+STORAGE_DIR = "storage"
+
+os.makedirs(STORAGE_DIR, exist_ok=True)
+
+def handle_client(conn, addr):
+  print(f"Connected client from {addr}")
+
+  try:
+    while True:
+      data = conn.recv(BUFFER_SIZE)
+
+      if not data:
+        break
+
+      message = data.decode().strip()
+      print(f"Received from {addr}: {message}")
+
+      if message.startswith("/list"):
+        files = os.listdir(STORAGE_DIR)
+        if not files:
+          conn.sendall(b"empty")
+        else:
+          conn.sendall("\n".join(files).encode())
+
+      elif message.startswith("/upload"):
+        parts = message.split()
+        if len(parts) < 2:
+          conn.sendall(b"error: filename required")
+          continue
+
+        filename = parts[1]
+        
+        # ack command from client
+        conn.sendall(b"ok\n")
+
+        data_size = conn.recv(BUFFER_SIZE)
+        filesize = int(data_size.decode())
+
+        filepath = os.path.join(STORAGE_DIR, filename)
+
+        with open(filepath, "wb") as f:
+          received = 0
+          while received < filesize:
+            chunk = conn.recv(BUFFER_SIZE)
+            if not chunk:
+              break
+            f.write(chunk)
+            received += len(chunk)
+
+        print(f"Uploaded {filename} from {addr} successfully")
+        conn.sendall(b"Uploaded successfully")
+
+      elif message.startswith("/download"):
+        parts = message.split()
+        if len(parts) < 2:
+          conn.sendall(b"error: filename required")
+          continue
+
+        filename = parts[1]
+        filepath = os.path.join(STORAGE_DIR, filename)
+
+        if not os.path.exists(filepath):
+          conn.sendall(b"0")
+          continue
+
+        filesize = os.path.getsize(filepath)
+        conn.sendall(str(filesize).encode())
+
+        # wait for client ack
+        ack = conn.recv(BUFFER_SIZE)
+
+        with open(filepath, "rb") as f:
+          while True:
+            chunk = f.read(BUFFER_SIZE)
+            if not chunk:
+              break
+            conn.sendall(chunk)
+
+        print(f"Downloaded {filename} to {addr}  successfully")
+        conn.sendall(b"Downloaded successfully")
+
+      else:
+        response = f"{message}"
+        conn.sendall(response.encode())
+
+  except Exception as err:
+    print(f"Error on {addr}: {err}")
+
+  finally:
+    conn.close()
+    print(f"{addr} connection disconnected")
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen(1)  
+
+    print(f"Server running on {HOST}:{PORT}")
+
+    while True:
+        conn, addr = server.accept()
+        handle_client(conn, addr) 
+
+if __name__ == "__main__":
+    main()
